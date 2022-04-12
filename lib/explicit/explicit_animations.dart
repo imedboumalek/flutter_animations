32c0e1b7c1b1f6921dd4b5fcaea7af83ad8465d4
Imed Boumalek <imedboumalek@gmail.com>
2022-04-12 23:17:34 +0100

    added Explicit Animations

--- lib/explicit/explicit_animations.dart
+++ lib/explicit/explicit_animations.dart
@@ -35,14 +35,58 @@ class FooTransitionPage extends StatefulWidget {
   State<FooTransitionPage> createState() => _FooTransitionPageState();
 }
 
-class _FooTransitionPageState extends State<FooTransitionPage> {
+class _FooTransitionPageState extends State<FooTransitionPage>
+    with SingleTickerProviderStateMixin<FooTransitionPage> {
+  late final AnimationController _controller;
+  late final Animation<double> _animation;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    _animation = Tween<double>(begin: 0.2, end: 1).animate(_controller);
+
+    _animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        _controller.forward();
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('FooTransition'),
+      appBar: AppBar(
+        title: const Text('FooTransition'),
+      ),
+      body: Center(
+        child: ScaleTransition(
+          scale: _animation,
+          child: Container(
+            decoration: BoxDecoration(color: Colors.green),
+            width: 300,
+            height: 300,
+          ),
         ),
-        body: SizedBox());
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _controller.isAnimating ? _controller.stop() : _controller.forward();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
 
